831b921f9228183acb0c97b60b564787c09cdb95
piotr-j <me@piotrjastrzebski.io>
2016-11-14 17:46:19 +0100

    light now has a dispose check

--- src/box2dLight/Light.java
+++ src/box2dLight/Light.java
@@ -17,9 +17,9 @@ import com.badlogic.gdx.utils.IntArray;
  * Light is data container for all the light parameters. When created lights
  * are automatically added to rayHandler and could be removed by calling
  * {@link #remove()} and added manually by calling {@link #add(RayHandler)}.
- * 
+ *
  * <p>Implements {@link Disposable}
- * 
+ *
  * @author kalle_h
  */
 public abstract class Light implements Disposable {
@@ -28,12 +28,12 @@ public abstract class Light implements Disposable {
 	static final float zeroColorBits = Color.toFloatBits(0f, 0f, 0f, 0f);
 	static final float oneColorBits = Color.toFloatBits(1f, 1f, 1f, 1f);
 	static final int MIN_RAYS = 3;
-	
+
 	protected final Color color = new Color();
 	protected final Vector2 tmpPosition = new Vector2();
-	
+
 	protected RayHandler rayHandler;
-	
+
 	protected boolean active = true;
 	protected boolean soft = true;
 	protected boolean xray = false;
@@ -44,12 +44,12 @@ public abstract class Light implements Disposable {
 
 	protected int rayNum;
 	protected int vertexNum;
-	
+
 	protected float distance;
 	protected float direction;
 	protected float colorF;
 	protected float softShadowLength = 2.5f;
-	
+
 	protected Mesh lightMesh;
 	protected Mesh softShadowMesh;
 
@@ -81,7 +81,7 @@ public abstract class Light implements Disposable {
 	/**
 	 * Creates new active light and automatically adds it to the specified
 	 * {@link RayHandler} instance.
-	 * 
+	 *
 	 * @param rayHandler
 	 *            not null instance of RayHandler
 	 * @param rays
@@ -92,7 +92,7 @@ public abstract class Light implements Disposable {
 	 * @param distance
 	 *            light distance (if applicable), soft shadow length is set to distance * 0.1f
 	 * @param directionDegree
-	 *            direction in degrees (if applicable) 
+	 *            direction in degrees (if applicable)
 	 */
 	public Light(RayHandler rayHandler, int rays, Color color,
 				 float distance, float directionDegree) {
@@ -126,7 +126,7 @@ public abstract class Light implements Disposable {
 
 	/**
 	 * Sets light distance
-	 * 
+	 *
 	 * <p>NOTE: MIN value should be capped to 0.1f meter
 	 */
 	public abstract void setDistance(float dist);
@@ -135,34 +135,34 @@ public abstract class Light implements Disposable {
 	 * Sets light direction
 	 */
 	public abstract void setDirection(float directionDegree);
-	
+
 	/**
 	 * Attaches light to specified body
-	 * 
+	 *
 	 * @param body
 	 *            that will be automatically followed, note that the body
 	 *            rotation angle is taken into account for the light offset
 	 *            and direction calculations
 	 */
 	public abstract void attachToBody(Body body);
-	
+
 	/**
 	 * @return attached body or {@code null}
-	 * 
+	 *
 	 * @see #attachToBody(Body)
 	 */
 	public abstract Body getBody();
 
 	/**
 	 * Sets light starting position
-	 * 
+	 *
 	 * @see #setPosition(Vector2)
 	 */
 	public abstract void setPosition(float x, float y);
 
 	/**
 	 * Sets light starting position
-	 * 
+	 *
 	 * @see #setPosition(float, float)
 	 */
 	public abstract void setPosition(Vector2 position);
@@ -176,7 +176,7 @@ public abstract class Light implements Disposable {
 	 * @return vertical starting position of light in world coordinates
 	 */
 	public abstract float getY();
-	
+
 	/**
 	 * @return starting position of light in world coordinates
 	 *         <p>NOTE: changing this vector does nothing
@@ -184,15 +184,15 @@ public abstract class Light implements Disposable {
 	public Vector2 getPosition() {
 		return tmpPosition;
 	}
-	
+
 	/**
 	 * Sets light color
-	 * 
+	 *
 	 * <p>NOTE: you can also use colorless light with shadows, e.g. (0,0,0,1)
-	 * 
+	 *
 	 * @param newColor
 	 *            RGB set the color and Alpha set intensity
-	 * 
+	 *
 	 * @see #setColor(float, float, float, float)
 	 */
 	public void setColor(Color newColor) {
@@ -207,9 +207,9 @@ public abstract class Light implements Disposable {
 
 	/**
 	 * Sets light color
-	 * 
+	 *
 	 * <p>NOTE: you can also use colorless light with shadows, e.g. (0,0,0,1)
-	 * 
+	 *
 	 * @param r
 	 *            lights color red component
 	 * @param g
@@ -218,7 +218,7 @@ public abstract class Light implements Disposable {
 	 *            lights color blue component
 	 * @param a
 	 *            lights shadow intensity
-	 * 
+	 *
 	 * @see #setColor(Color)
 	 */
 	public void setColor(float r, float g, float b, float a) {
@@ -226,7 +226,7 @@ public abstract class Light implements Disposable {
 		colorF = color.toFloatBits();
 		if (staticLight) dirty = true;
 	}
-	
+
 	/**
 	 * Adds light to specified RayHandler
 	 */
@@ -245,7 +245,7 @@ public abstract class Light implements Disposable {
 	public void remove() {
 		remove(true);
 	}
-	
+
 	/**
 	 * Removes light from specified RayHandler and disposes it if requested
 	 */
@@ -264,8 +264,14 @@ public abstract class Light implements Disposable {
 	 */
 	public void dispose() {
 		affectedFixtures.clear();
-		lightMesh.dispose();
-		softShadowMesh.dispose();
+		if (lightMesh != null) {
+			lightMesh.dispose();
+			lightMesh = null;
+		}
+		if (softShadowMesh != null) {
+			softShadowMesh.dispose();
+			softShadowMesh = null;
+		}
 		for (Mesh mesh : dynamicShadowMeshes) {
 			mesh.dispose();
 		}
@@ -289,7 +295,7 @@ public abstract class Light implements Disposable {
 		this.active = active;
 		if (rayHandler == null)
 			return;
-		
+
 		if (active) {
 			rayHandler.lightList.add(this);
 			rayHandler.disabledLights.removeValue(this, true);
@@ -308,10 +314,10 @@ public abstract class Light implements Disposable {
 
 	/**
 	 * Enables/disables x-ray beams for this light
-	 * 
+	 *
 	 * <p>Enabling this will allow beams go through obstacles that reduce CPU
 	 * burden of light about 70%.
-	 * 
+	 *
 	 * <p>Use the combination of x-ray and non x-ray lights wisely
 	 */
 	public void setXray(boolean xray) {
@@ -332,11 +338,11 @@ public abstract class Light implements Disposable {
 
 	/**
 	 * Enables/disables this light static behavior
-	 * 
+	 *
 	 * <p>Static light do not get any automatic updates but setting any
 	 * parameters will update it. Static lights are useful for lights that you
 	 * want to collide with static geometry but ignore all the dynamic objects
-	 * 
+	 *
 	 * <p>Reduce CPU burden of light about 90%
 	 */
 	public void setStaticLight(boolean staticLight) {
@@ -369,14 +375,14 @@ public abstract class Light implements Disposable {
 
 	/**
 	 * Sets softness value for beams tips
-	 * 
+	 *
 	 * <p>Default: {@code 2.5f}
 	 */
 	public void setSoftnessLength(float softShadowLength) {
 		this.softShadowLength = softShadowLength;
 		if (staticLight) dirty = true;
 	}
-	
+
 	/**
 	 * @return current color of this light
 	 */
@@ -400,25 +406,25 @@ public abstract class Light implements Disposable {
 
 	/**
 	 * Checks if given point is inside of this light area
-	 * 
+	 *
 	 * @param x - horizontal position of point in world coordinates
 	 * @param y - vertical position of point in world coordinates
 	 */
 	public boolean contains(float x, float y) {
 		return false;
 	}
-	
+
 	/**
 	 * Sets if the attached body fixtures should be ignored during raycasting
-	 * 
+	 *
 	 * @param flag - if {@code true} all the fixtures of attached body
 	 *               will be ignored and will not create any shadows for this
-	 *               light. By default is set to {@code false}. 
+	 *               light. By default is set to {@code false}.
 	 */
 	public void setIgnoreAttachedBody(boolean flag) {
 		ignoreBody = flag;
 	}
-	
+
 	/**
 	 * @return if the attached body fixtures will be ignored during raycasting
 	 */
@@ -445,8 +451,8 @@ public abstract class Light implements Disposable {
 		my = new float[vertexNum];
 		f = new float[vertexNum];
 	}
-	
-	/** 
+
+	/**
 	 * @return number of rays set for this light
 	 */
 	public int getRayNum()
@@ -463,16 +469,16 @@ public abstract class Light implements Disposable {
 		@Override
 		final public float reportRayFixture(Fixture fixture, Vector2 point,
 				Vector2 normal, float fraction) {
-			
+
 			if ((globalFilterA != null) && !globalContactFilter(fixture))
 				return -1;
-			
+
 			if ((filterA != null) && !contactFilter(fixture))
 				return -1;
-			
+
 			if (ignoreBody && fixture.getBody() == getBody())
 				return -1;
-			
+
 			// if (fixture.isSensor())
 			// return -1;
 			mx[m_index] = point.x;
@@ -481,7 +487,7 @@ public abstract class Light implements Disposable {
 			return fraction;
 		}
 	};
-	
+
 	boolean contactFilter(Fixture fixtureB) {
 		Filter filterB = fixtureB.getFilterData();
 
@@ -502,7 +508,7 @@ public abstract class Light implements Disposable {
 
 	/**
 	 * Creates new contact filter for this light with given parameters
-	 * 
+	 *
 	 * @param categoryBits - see {@link Filter#categoryBits}
 	 * @param groupIndex   - see {@link Filter#groupIndex}
 	 * @param maskBits     - see {@link Filter#maskBits}
@@ -535,7 +541,7 @@ public abstract class Light implements Disposable {
 
 	/**
 	 * Creates new contact filter for ALL LIGHTS with give parameters
-	 * 
+	 *
 	 * @param categoryBits - see {@link Filter#categoryBits}
 	 * @param groupIndex   - see {@link Filter#groupIndex}
 	 * @param maskBits     - see {@link Filter#maskBits}
